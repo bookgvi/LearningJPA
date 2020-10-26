d9653236281bf6bff5b08fbd8057882f45f3de2d
bookgvi <bookgvi@gmail.com>
2020-10-26 13:59:08 +0300

    Overriding hasCode() and equals() in entity

--- src/main/java/localhost/models/Employee.java
+++ src/main/java/localhost/models/Employee.java
@@ -3,24 +3,23 @@ package localhost.models;
 import javax.persistence.*;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import java.util.Objects;
 
 @Entity
 public class Employee {
   /**
    * entityId = START_WITH - allocationSize + INCREMENT_BY
    *
-   * @SequenceGenerator(
-   *     name="email-seq-gen",
-   *     sequenceName="EMAIL_SEQ_GEN",
-   *     allocationSize=500
+   * @SequenceGenerator( name="email-seq-gen",
+   * sequenceName="EMAIL_SEQ_GEN",
+   * allocationSize=500
    * ) // initialValue=1 (default)
-   *
+   * <p>
    * CREATE SEQUENCE EMAIL_SEQ_GEN START WITH 1 INCREMENT BY 500;
-   *
-   *  produces
-   *
+   * <p>
+   * produces
+   * <p>
    * entityId = 1 - 500 + 1 = -500 // EclipseLink error
-   *
    */
   @Id
   @SequenceGenerator(
@@ -28,7 +27,7 @@ public class Employee {
     sequenceName = "employee_id_seq",
     allocationSize = 1
   )
-  @GeneratedValue(generator ="employee_id", strategy = GenerationType.SEQUENCE)
+  @GeneratedValue(generator = "employee_id", strategy = GenerationType.SEQUENCE)
   private long id;
   @NotNull
   private String name;
@@ -68,5 +67,28 @@ public class Employee {
       "id: " + this.id +
       "\n name: " + this.name +
       "\n salary: " + this.salary + "\n";
+  }
+
+  @Override
+  public int hashCode() {
+    int prime = 666;
+    int hash = 777;
+    hash = prime * hash + (int) this.id;
+    hash = prime * hash + Objects.hashCode(this.name);
+    hash = prime * hash + this.salary;
+    return hash;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null) return false;
+    if (getClass() != obj.getClass()) return false;
+
+    final Employee someEmp = (Employee) obj;
+    if (this.id != someEmp.id) return false;
+    if (!this.name.equals(someEmp.name)) return false;
+    if (this.salary != someEmp.salary) return false;
+    return true;
   }
 }
