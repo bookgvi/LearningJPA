9cdb1b228a80e0bd972a87458a6994a0554b545c
bookgvi <bookgvi@gmail.com>
2020-10-27 20:40:50 +0300

    Only one EmbeddedId may be per Entity

--- src/main/java/localhost/models/News.java
+++ src/main/java/localhost/models/News.java
@@ -8,7 +8,6 @@ import java.util.Date;
 @Entity
 public class News {
   private KeyForNews id;
-  private KeyForNews lang;
   private String title;
   private Date date;
 
@@ -19,15 +18,6 @@ public class News {
   @EmbeddedId
   public KeyForNews getId() {
     return this.id;
-  }
-
-  public void setLang(KeyForNews lang) {
-    this.lang = lang;
-  }
-
-  @EmbeddedId
-  public KeyForNews getLang() {
-    return this.lang;
   }
 
   public void setTitle(String title) {
